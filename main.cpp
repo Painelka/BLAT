16976e52776053d530b5a24843b53ba594dd8f49
PAIN <113128986+Painelka@users.noreply.github.com>
2023-11-18 11:47:48 +0200

    erte

--- main.cpp
+++ main.cpp
@@ -1,193 +1,272 @@
 ﻿#include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
-#include "move.hpp"
+#include <iostream>
 #include <vector>
 #include <chrono>
-using namespace sf;
+#include "move.h"
+#include "menu.h"
+#include "locale.h"
 
+using namespace sf;
 float kadr = 1; // кадр анимации
 
-int main(){
-    setlocale(LC_ALL, "Russian");
-    RenderWindow window(VideoMode(1920, 1080), "Lire");
-    Texture texture; // фон
-    Sprite Fon;
-    texture.loadFromFile("assets/Fon.png");
-    Fon.setTexture(texture);
+int main()
+{
+    setlocale(LC_ALL, "RUS");
 
-    lol::player boy(200, 450, 48, 80, "player.png", 1, "Лирэ");
-    lol::npc npc (30, 450, 36, 82, "npc.png", 1);
-    lol::cat cat (1400, 370, 74, 120, "bananacat.png");
+    //MENU window
+    RenderWindow MENU(VideoMode(1920, 1080), L"Меню");
+    Menu menu(MENU.getSize().x, MENU.getSize().y);
+    Music Intro;
+    Music Sound;
+    Texture menubg;
+    menubg.loadFromFile("assets/menubg.png");
+    Sprite bg;
+    bg.setTexture(menubg);
+
+    if (!Intro.openFromFile("assets/intro.ogg")) { cout << L"Файл не найден :(" << endl; }
+    if (!Sound.openFromFile("assets/sound.ogg")) { cout << L"Файл не найден :(" << endl; }
+    // MUSIC FROM MENU
+    Intro.play();
+    Intro.setLoop(true);
+    while (MENU.isOpen())
+    {
+        Sound.play(); //playing while game and about
+        Sound.setLoop(true);
+        Event event;
+        while (MENU.pollEvent(event))
+        {
+            if (event.type == Event::Closed) { MENU.close(); }
+
+            if (event.type == Event::KeyPressed)
+            {
+                if ((event.key.code == Keyboard::W) || (event.key.code == Keyboard::Up))
+                {
+                    menu.MoveUp();
+                    break;
+                }
+                if ((event.key.code == Keyboard::S) || (event.key.code == Keyboard::Down))
+                {
+                    menu.MoveDown();
+                    break;
+                }
+
+                if (event.key.code == Keyboard::Return)
+                {
+                    RenderWindow Game(VideoMode(1920, 1080), L"Мир Висельника");
+                    Texture texture; // фон game
+                    Sprite Fon;
+                    texture.loadFromFile("assets/pink.png");
+                    Fon.setTexture(texture);
+
+                    RenderWindow About(VideoMode(1920, 1080), L"Подробнее об игре");
+                    Texture tt; // фон about game
+                    Sprite bgAbout;
+                    tt.loadFromFile("assets/about.png");
+                    bgAbout.setTexture(tt);
+
+                    int x = menu.pressed();
+                    Intro.stop();
+                    if (x == 0)
+                    {
+                        lol::player GG(950, 600, 96, 160, "gg.png", 1); //локация гг х и у
+                        lol::npc npc(500, 300, 36, 82, "npc.png", 1);
+                        lol::cat cat(1400, 370, 74, 120, "bananacat.png");
+
+                        Texture textbox_texture;
+                        Sprite textbox_sprite;
+                        textbox_texture.loadFromFile("assets/Box.png");
+                        textbox_sprite.setTexture(textbox_texture);
 
 
+                        Font font;
+                        if (!font.loadFromFile("assets/pixy.ttf")) { cout << L"Файл не найден :(" << endl; }
 
+                        Text text;
+                        text.setFont(font);
+                        text.setString(L"Лирэ");
+                        text.setCharacterSize(70);
+                        text.setPosition(155, 680);
 
-    Texture textbox_texture;
-    Sprite textbox_sprite;
-    textbox_texture.loadFromFile("text/Box.png");
-    textbox_sprite.setTexture(textbox_texture);
+                        Text text3;
+                        text3.setFont(font);
+                        text3.setString(L"Мэршон");
+                        text3.setCharacterSize(70);
+                        text3.setPosition(155, 680);
 
-    sf::Font font;
-    if (!font.loadFromFile("text/thin_pixel-7.ttf")) {
-        // Обработка ошибки при загрузке шрифта
-        return -1;
-    }
+                        Text text1;
+                        text1.setFont(font);
+                        text1.setString(L"Кот");
+                        text1.setCharacterSize(70);
+                        text1.setPosition(200, 800);
 
+                        Clock clock;
 
-    sf::Text text;
-    text.setFont(font);
-    text.setString(L"Лирэ");
-    text.setCharacterSize(70);
-    text.setPosition(155, 680);
+                        while (Game.isOpen())
+                        {
+                            float time = clock.getElapsedTime().asMicroseconds();
+                            clock.restart();
+                            time = time / 1500;
+                            Event aevent;
+                            while (Game.pollEvent(aevent))
+                            {
+                                if (aevent.type == Event::Closed) { Game.close(); }
+                                if (aevent.type == Event::KeyPressed)
+                                {
+                                    if (aevent.key.code == Keyboard::Escape) { Game.close(); }
+                                }
+                            }
 
+                            GG.stopped(1000, 48 * 2, 80 * 2, 800);
+                            //npc.stopped(1000, 36*2, 82*2, 800);
 
-    sf::Text text3;
-    text3.setFont(font);
-    text3.setString(L"Мэршон");
-    text3.setCharacterSize(70);
-    text3.setPosition(155, 680);
+                            GG.update(time);
+                            if (Keyboard::isKeyPressed(Keyboard::A)) {
+                                GG.dir = 0;
+                                GG.m_speed = 0.3;
+                                kadr = kadr + 0.01 * time;
+                                if (kadr > 4) {
+                                    kadr = kadr - 4;
+                                }
+                                GG.sprite.setTextureRect(IntRect(48 * 2 * int(kadr), 240 * 2, 48 * 2, 80 * 2));
+                            }
+                            else if (Keyboard::isKeyPressed(Keyboard::D)) {
+                                GG.dir = 1;
+                                GG.m_speed = 0.3;
+                                kadr = kadr + 0.01 * time;
+                                if (kadr > 4) {
+                                    kadr = kadr - 4;
+                                }
+                                GG.sprite.setTextureRect(IntRect(48 * 2 * int(kadr), 160 * 2, 48 * 2, 80 * 2));
+                            }
+                            else if (Keyboard::isKeyPressed(Keyboard::W)) {
+                                GG.dir = 2;
+                                GG.m_speed = 0.3;
+                                kadr = kadr + 0.01 * time;
+                                if (kadr > 4) {
+                                    kadr = kadr - 4;
+                                }
+                                GG.sprite.setTextureRect(IntRect(48 * 2 * int(kadr), 80 * 2, 48 * 2, 80 * 2));
+                            }
+                            else if (Keyboard::isKeyPressed(Keyboard::S)) {
+                                GG.dir = 3;
+                                GG.m_speed = 0.3;
+                                kadr = kadr + 0.01 * time;
+                                if (kadr > 4) {
+                                    kadr = kadr - 4;
+                                }
+                                GG.sprite.setTextureRect(IntRect(48 * 2 * int(kadr), 0, 48 * 2, 80 * 2));
+                            }
+                            if (GG.perehod(80, 800)) {
+                                Sprite Pink;
+                                texture.loadFromFile("assets/bg2.png");
+                                Pink.setTexture(texture);
+                                Game.clear();
+                                Game.draw(Pink);
+                                Game.draw(GG.sprite);
+                                npc.npc_sprite.setPosition(-205, -30);
+                                cat.npc1_sprite.setPosition(-205, -300);
 
-    sf::Text text1;
-    text1.setFont(font);
-    text1.setString(L"ertalalla");
+                            }
+                            if (GG.granics(80, 800)) {
+                                texture.loadFromFile("assets/pink.png");
+                                Fon.setTexture(texture);
+                                Game.clear();
+                                Game.draw(Fon);
+                                Game.draw(GG.sprite);
+                                npc.npc_sprite.setPosition(500, 300);//
+                                Game.draw(npc.npc_sprite);
+                                cat.npc1_sprite.setPosition(1400, 370);
+                                Game.draw(cat.npc1_sprite);
+                            }
 
-    text1.setCharacterSize(55);
-    text1.setPosition(200, 800);
+                            lol::Textbox Box(100, 500, 800, 300, "Box1.png", "HELP ME");
+                            vector<string> talk = { "Hello", "...", "Im not Fine",  "Im dead", "Because Matan" };
 
+                            float k = 0;
+                            if ((Keyboard::isKeyPressed(Keyboard::E)) && (GG.m_x <= npc.npc_x + 100) && (GG.m_y <= npc.npc_y + 100))
+                            {
+                                Game.draw(Box.textbox_sprite);
+                                Game.draw(text);
+                                Game.display();
+                                while ((k < 5))
+                                {
+                                    Game.draw(Box.textbox_sprite);
+                                    Game.draw(text3);
+                                    if (Keyboard::isKeyPressed(Keyboard::Q))
+                                    {
+                                        cout << k << endl;
+                                        text1.setString(talk[k]);
+                                        Game.draw(text1);
+                                        Game.display();
+                                        k = k + 0.25;
+                                    }
+                                }
+                            }
+                            float t = 0;
+                            vector<string> talk2 = { "Cat?", "Cute....?" };
+                            if ((Keyboard::isKeyPressed(Keyboard::E)) && ((cat.npc_x2 + 70) >= GG.m_x) && ((cat.npc_y2 + 70) >= GG.m_y)) {
+                                cat.said("meow.wav");
+                                cout << cat.npc_x2 << endl;
+                                cout << cat.npc_y2 << endl;
+                                cout << GG.m_x << endl;
+                                Game.draw(Box.textbox_sprite);
+                                Game.draw(text);
+                                Game.display();
+                                while ((t <= 1.99))
+                                {
+                                    Game.draw(Box.textbox_sprite);
+                                    Game.draw(text);
+                                    if (Keyboard::isKeyPressed(Keyboard::Q))
+                                    {
+                                        cout << t << endl;
+                                        text1.setString(talk2[t]);
+                                        Game.draw(text1);
+                                        Game.display();
+                                        t = t + 0.25;
+                                    }
+                                }
+                            }
+                            About.close();
+                            Intro.play(); // MUSIC FROM MENU IS PLAYING AGAIN
+                            Game.clear();
+                            Game.draw(Fon);
+                            Game.draw(npc.npc_sprite);
+                            Game.draw(cat.npc1_sprite);
+                            Game.draw(GG.sprite);
+                            Game.display();
+                        }
+                    }
 
+                    if (x == 1)
+                    {
+                        while (About.isOpen())
+                        {
+                            Event aevent;
+                            while (About.pollEvent(aevent))
+                            {
+                                if (aevent.type == Event::Closed) { About.close(); }
+                                if (aevent.type == Event::KeyPressed)
+                                {
+                                    if (aevent.key.code == Keyboard::Escape) { About.close(); }
+                                }
+                            }
 
+                            Game.close();
+                            Intro.play(); // MUSIC FROM MENU IS PLAYING AGAIN
+                            About.clear();
+                            About.draw(bgAbout);
+                            About.display();
+                        }
+                    }
 
-
-    Clock clock;
-    while (window.isOpen()){
-        float time = clock.getElapsedTime().asMicroseconds();
-        clock.restart();
-        time = time / 1500;
-        sf::Event event;
-        while (window.pollEvent(event)){
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        boy.stopped(1000, 48,80, 800);
-        
-        boy.update(time);
-        if (Keyboard::isKeyPressed(Keyboard::A)) {
-            boy.dir = 0;
-            boy.m_speed = 0.3;
-            kadr = kadr + 0.01 * time;
-            if (kadr > 4) {
-                kadr = kadr - 4;
-            }
-            boy.sprite.setTextureRect(IntRect(48 * int(kadr), 240, 48, 80));
-
-        }
-        else if (Keyboard::isKeyPressed(Keyboard::D)) {
-            boy.dir = 1;
-            boy.m_speed = 0.3;
-            kadr = kadr + 0.01 * time;
-            if (kadr > 4) {
-                kadr = kadr - 4;
-            }
-            boy.sprite.setTextureRect(IntRect(48 * int(kadr), 160, 48, 80));
-
-        }
-        else if (Keyboard::isKeyPressed(Keyboard::W)) {
-            boy.dir = 2;
-            boy.m_speed = 0.3;
-            kadr = kadr + 0.01 * time;
-            if (kadr > 4) {
-                kadr = kadr - 4;
-            }
-            boy.sprite.setTextureRect(IntRect(48 * int(kadr), 80, 48, 80));
-  
-        }
-        else if (Keyboard::isKeyPressed(Keyboard::S)) {
-            boy.dir = 3;
-            boy.m_speed = 0.3;
-            kadr = kadr + 0.01 * time;
-            if (kadr > 4) {
-                kadr = kadr - 4;
-            }
-            boy.sprite.setTextureRect(IntRect(48 * int(kadr), 0, 48, 80));
-
-        }
-        if (boy.perehod(80, 800)){
-            Sprite Pink;
-            texture.loadFromFile("assets/pink_2.png");
-            Pink.setTexture(texture);
-            window.clear();
-            window.draw(Pink);
-            window.draw(boy.sprite);
-            npc.npc_sprite.setPosition(-205, -30);
-            cat.npc1_sprite.setPosition(-205, -300);
-
-        }
-        if (boy.granics(80, 800)) {
-            texture.loadFromFile("assets/Fon.png");
-            Fon.setTexture(texture);
-            window.clear();
-            window.draw(Fon);
-            window.draw(boy.sprite);
-            npc.npc_sprite.setPosition(30, 450);
-            window.draw(npc.npc_sprite);
-            cat.npc1_sprite.setPosition(1400, 370);
-            window.draw(cat.npc1_sprite);
-        }
-        lol::Textbox Box(100, 500, 800, 300, "Box1.png", "HELP ME");
-        vector<string> talk = { "Hello", "...", "Im not Fine",  "Im dead", "Because Matan"};
-        float k = 0;
-        if ((Keyboard::isKeyPressed(Keyboard::E))&& (boy.m_x<=npc.npc_x+50)&& (boy.m_y <= npc.npc_y + 50)) {
-            window.draw(Box.textbox_sprite);
-            window.draw(text);
-            window.display();
-            while ((k<5)) {
-                window.draw(Box.textbox_sprite);
-                window.draw(text3);
-
-                if (Keyboard::isKeyPressed(Keyboard::Q)) {
-                  
-                    cout << k << endl;
-                    text1.setString(talk[k]);
-                    window.draw(text1);
-                    window.display();
-                    k = k + 0.006;
+                    if (x == 2) { MENU.close(); }
                 }
             }
         }
-        float t = 0;
-        vector<string> talk2 = { "Cat?", "Cute....?"};
-        if((Keyboard::isKeyPressed(Keyboard::E)) && ((cat.npc_x2 + 70) >= boy.m_x) && ((cat.npc_y2 + 70) >= boy.m_y )) {
-            cat.said("meow.wav");
-            cout << cat.npc_x2 << endl;
-            cout << cat.npc_y2 << endl;
-            cout << boy.m_x << endl;
-            window.draw(Box.textbox_sprite);
-            window.draw(text);
-            window.display();
-            while ((t <= 1.99)) {
-                window.draw(Box.textbox_sprite);
-                window.draw(text);
-                if (Keyboard::isKeyPressed(Keyboard::Q)) {
-                    cout << t << endl;
-                    text1.setString(talk2[t]);
-                    window.draw(text1);
-                    window.display();
-                    t = t + 0.25;
-                    
-                }
-            }
-        }
-
-       
-
-
-
-        window.clear();
-        window.draw(Fon);
-        window.draw(npc.npc_sprite);
-        window.draw(boy.sprite);
-        window.draw(cat.npc1_sprite);
-        window.display();
+        MENU.clear();
+        MENU.draw(bg); //bg first!
+        menu.draw(MENU);
+        MENU.display();
     }
-    return 0;
 }
